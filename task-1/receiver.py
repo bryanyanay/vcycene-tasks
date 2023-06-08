e61c9b1570399840087f88fa8d9157d907cf21d0
Bryan Yan <74737261+bryanyanay@users.noreply.github.com>
2023-06-07 22:09:31 -0400

    Add file writing to receiver
    After adding file writing, receiver seems to miss
    first few bytes of second successive data line input,
    so that it doesn't make 16 bytes and doesn't unpack.
    
    Also, maybe don't use pynput for keyboard, since it
    detects space even if window isn't in focus?

--- task-1/receiver.py
+++ task-1/receiver.py
@@ -1,16 +1,17 @@
 import serial
 import struct
 from pynput import keyboard
-from sys import exit
+from datetime import date
 
 
-port = "\\\\.\\CNCA0" # port to read from
-stopProgram = False
+port = "\\\\.\\CNCA0"       # port to read from
+filePath = "dataRecord.txt" # file to write to
 
 def readLine(port):
   """
   reads 16 bytes from port
    - if less than 16 bytes are read, returns False
+   - otherwise returns dictionary of the data
   """
   with serial.Serial(port, timeout=2) as s:
     print("\n>>> receiving from ", port, " ...")
@@ -18,6 +19,7 @@ def readLine(port):
     print("received: ", dataBin)
 
   if len(dataBin) != 16:
+    print("NOT 16 bytes!")
     return False
   
   data = struct.unpack("<bbbHHIIx", dataBin)
@@ -31,6 +33,15 @@ def readLine(port):
   print("unpacked to: ", dataDict)
   return dataDict
 
+def writeLine(filePath, data):
+  """
+  Writes a data line to the file at filePath
+   - appends the new data
+  Data from the line is taken from the dictionary
+  """
+  with open(filePath, "a") as file:
+    file.write(f'{date.today()}-{data["temps"][0]},{data["temps"][1]},{data["temps"][2]},{data["pres"][0]},{data["pres"][1]},{data["gasres"][0]},{data["gasres"][1]}')
+
 def onPress(key):
   if key == keyboard.Key.space:
     return False 
@@ -39,7 +50,10 @@ listener = keyboard.Listener(on_press=onPress)
 listener.start()
 
 while True:
-  readLine(port)
+  dataDict = readLine(port)
+  if dataDict:
+    writeLine(filePath, dataDict)
+
   if not listener.is_alive():
     print("\n>>> Space key pressed, exiting program\n")
     break
